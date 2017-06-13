a30d76f9b2f4f67db244e4af26c81cc19e9ddc80
hgrewal1 <harvinder.grewal@hotmail.com>
2017-06-12 21:22:36 -0400

    grewal

---
+++ src/java/utilities/create_connection.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import java.sql.*;
+
+
+
+
+
+public class create_connection {
+
+
+  Statement stmt = null;
+    //JDBC driver name and database URL
+     final String driverName = "oracle.jdbc.OracleDriver";
+     final String url = "jdbc:oracle:thin:@localhost:1521:XE" ;
+    
+    // Database credentials
+     final String username = "grewal";
+     final String password = "cegepgim";
+private  Connection con;
+  public  Connection getConnection() throws ClassNotFoundException, SQLException {
+     
+            Class.forName(driverName);
+          
+                con = DriverManager.getConnection(url, username, password);
+       
+        return con;
+    }
+   public  void closeConnection() throws SQLException {
+      con.close();
+  }
+  
+  public  void closeStmt() throws SQLException {
+      stmt.close();
+  }
+     public  ResultSet grewal(String S) throws SQLException {
+         
+stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+return stmt.executeQuery(S);
+}
+}
